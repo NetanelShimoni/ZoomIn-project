bef3752f2c7dd4b9a099d2f562edc33785507fa6
Netanel Shimoni <netanelsh0@gmail.com>
2022-11-18 12:28:03 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import FilmData from './components/FilmData';
+import TOC from './components/TOC';
+import axois from "axios"
+import {  IResult } from './types/types';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axois.get('https://swapi.dev/api/films/');
+      const films = data.results;
+      setFilms(films)
+    }
+    fetchData().catch(console.error);
+
+  }, [])
+
+
+
+  const [films, setFilms] = useState<IResult[] | undefined>(undefined);
+  const [chosenFilm, setChosenFilm] = useState<IResult | undefined>(undefined);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid'>
+      <div style={{
+        display: "flex", minHeight: "100%"
+        , flexDirection: "column", padding: "10px",
+        color: "#ffff", justifyContent: 'center',
+      }}>
+        <TOC films={films} setChosenFilm={setChosenFilm} />
+      </div>
+      {films? <FilmData chosenFilm={chosenFilm}  /> : <></>}
+      
     </div>
   );
 }
 
 export default App;
+function uaeEffect(arg0: () => void, arg1: never[]) {
+  throw new Error('Function not implemented.');
+}
+
